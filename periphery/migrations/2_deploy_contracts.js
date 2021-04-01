4d2ad9c19e8ac08fa1e65d33d03ebe7c9d5da203
umilton-git <ulysses.github@protonmail.com>
2021-04-01 16:04:36 -0500

    Initial version

---
+++ periphery/migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const Router = artifacts.require("ChimpswapV2Router02.sol");
+const WETH = artifacts.require("WETH.sol");
+
+module.exports = async function (deployer, network) {
+  let weth;
+  const FACTORY_ADDRESS = '0xC8D328C498EA246A37E945bE4d06A03FF366757E';
+
+  if(network === 'mainnet'){
+     weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+  } else {
+    await deployer.deploy(WETH);
+    weth = await WETH.deployed();
+  }
+
+  await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
+};
